73283e2a33a661ebd9891bc4f4a8fdc6bb37c616
Ricardo Boss <contact@ricardoboss.de>
2019-12-04 23:46:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,155 @@
+# laravel-roles-migrations
+
+Adds migrations for [`jeremykenedy/laravel-roles`](https://github.com/jeremykenedy/laravel-roles).
+
+## Overview
+
+This package aims to provide migration functionality for roles and
+permissions (and their relationships).
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+    1. [Creating Migrations](#creating-migrations)
+    2. [Writing Migrations](#writing-migrations)
+    3. [Examples](#examples)
+
+## Installation
+
+Simply require this package with composer:
+
+```bash
+composer require ricardoboss/laravel-roles-migrations
+```
+
+## Usage
+
+### Creating Migrations
+
+This package adds a new abstract class - `RolesMigration` - which you
+can use to write migrations for your roles.
+
+> Right now, the process of creating roles migrations requires many manual
+> steps. Dedicated commands are on the to-do list.
+
+To add a new migration, simply execute
+
+```bash
+php artisan make:migration MyNewRolesMigration
+```
+
+to add a new migration (which can be found in `database/migrations/xxx_xx_xx_xxxxxx_my_new_roles_migration.php`).
+
+Open the migration and replace the content with this (substitute the class name):
+
+```php
+<?php
+
+use Ricardoboss\Database\Migrations\RolesMigration;
+
+class MyNewRolesMigration extends RolesMigration
+{
+    protected $permissions = [];
+
+    protected $roles = [];
+
+    protected $toAttach = [];
+
+    protected $toDetach = [];
+}
+
+```
+Notice how the migration class `extends` the new `RolesMigration` class instead
+of the default `Migration` class from Laravel.
+
+Now comes the interesting part, writing the migrations.
+
+### Writing Migrations
+
+A roles migration consists of four protected arrays which define what the
+migration does:
+
+* `$permissions`: contains permission definitions which shall be added
+* `$roles`: contains role definitions which shall be added
+* `$toAttach`: defines which permissions shall be attached to what role
+* `$toDetach`: defines which permissions shall be detached from what role
+
+An example of each migration type and their outcome can be found here:
+
+### Examples
+
+#### Permissions
+
+```php
+$permissions = [
+    [
+        'name' => "List users",
+        'slug' => "users.list", // optional
+        'description' => "Permission to list all users."
+    ]
+];
+```
+
+The `slug` value is optional.
+It will be derived from the name if missing.
+
+When the migration is executed, the above configuration will add a new permission
+to your database. This exact permission will be removed in case of a rollback.
+
+#### Roles
+
+```php
+$roles = [
+    [
+        'name' => "Admin",
+        'level' => 10,
+        'slug' => "admin", // optional
+        'description' => "A user with all available permissions." // optional
+    ]
+];
+```
+
+The `slug` and `description` values are optional. If `slug` is missing, it will
+be derived from the name. The default value for `description` is `null`.
+
+#### ToAttach & ToDetach
+
+```php
+$toAttach = [
+    'admin' => [
+        'users.list',
+        // more permissions...
+    ]
+];
+```
+
+`$toAttach` and `$toDetach` have the same structure.
+They consist of nested arrays which declare what role gets or looses which permissions.
+
+The key of the array must be the `slug` of a role whereas the value must be an array
+of permission `slug`s. You can list as many permissions as you want for each role.
+
+> The order in which permissions are attached and detached matters:
+> if you have the same configuration in both `$toAttach` and `$toDetach`,
+> the outcome will be that nothing changed since the permissions are first
+> attached and _then_ detached.
+
+## Contributing
+
+Feel free to fork the repository and create a pull request.
+You are encouraged to adhere to the [PSR-12](https://www.php-fig.org/psr/psr-12/)
+coding style guide.
+
+### To-Do List
+
+- [x] Basic migrations and rollbacks
+- [ ] `artisan` commands
+- [ ] support for updating existing roles/permissions
+
+If you see a missing feature you want or find a bug, please create an issue and describe it.
+
+---
+
+Thanks to [jeremykenedy](https://github.com/jeremykenedy) for creating the
+[`laravel-roles`](https://github.com/jeremykenedy/laravel-roles) package and of
+course [taylorotwell](https://github.com/taylorotwell) for creating such an amazing
+framework.
---
+++ src/Database/Migrations/RolesMigration.php
@@ -0,0 +1,164 @@
+<?php
+declare(strict_types=1);
+
+namespace ricardoboss\Database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
+use jeremykenedy\LaravelRoles\Models\Permission;
+use jeremykenedy\LaravelRoles\Models\Role;
+
+/**
+ * Class RolesMigration
+ *
+ * @package Ricardoboss\Database\Migrations
+ */
+abstract class RolesMigration extends Migration
+{
+    /**
+     * An array containing role definitions. A role definition is an array which contains at least these keys:
+     *   - name: (string) The name of the new role
+     *   - description: (string) A brief description of the role
+     *   - level: (int) The level of the role relative to the other roles
+     *
+     * Optionally, you can add the key 'slug' which is a url-safe key for the role.
+     *
+     * @var mixed[][]
+     */
+    protected $roles = [];
+
+    /**
+     * An array of permission definitions. A permission definition is an array which contains at least these keys:
+     *   - name: The name of the new permission
+     *   - description: A brief description of the permission
+     *
+     * Optionally, you can add the key 'slug' which is a url-safe key for the permission.
+     *
+     * @var string[][]
+     */
+    protected $permissions = [];
+
+    /**
+     * A key-value map that maps which permissions shall be attached to roles.
+     * The key is the role slug and the value is an array with permission slugs.
+     *
+     * @var string[][]
+     */
+    protected $toAttach = [];
+
+    /**
+     * A key-value map that maps which permissions shall be detached from roles.
+     * The key is the role slug and the value is an array with permission slugs.
+     *
+     * @var string[][]
+     */
+    protected $toDetach = [];
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // TODO: update existing models (levels, names, descriptions)
+        $this->addPermissions();
+        $this->addRoles();
+        $this->syncPermissions('attachPermission', $this->toAttach);
+        $this->syncPermissions('detachPermission', $this->toDetach);
+    }
+
+    /**
+     * Add new permissions.
+     */
+    public function addPermissions(): void
+    {
+        foreach ($this->permissions as $permission) {
+            if (!array_key_exists('slug', $permission))
+                $permission['slug'] = Str::slug($permission['name'], '.');
+
+            // do not re-create existing models
+            if (config('roles.models.permission')::where('slug', $permission['slug'])->exists())
+                continue;
+
+            config('roles.models.permission')::create($permission);
+        }
+    }
+
+    /**
+     * Add new roles.
+     */
+    public function addRoles(): void
+    {
+        foreach ($this->roles as $role) {
+            if (!array_key_exists('slug', $role))
+                $role['slug'] = Str::slug($role['name'], '.');
+
+            config('roles.models.role')::create($role);
+        }
+    }
+
+    /**
+     * Synchronize permissions with roles.
+     *
+     * @param string $method The method to call on the role. Can be either 'attachPermission' or 'detachPermission'
+     * @param string[][] $toSync Permissions to sync to a role. ['role' => ['permission1', 'permission2']]
+     */
+    public function syncPermissions(string $method, $toSync): void
+    {
+        foreach ($toSync as $roleSlug => $permissionSlugs) {
+            /** @var Role $role */
+            $role = config('roles.models.role')::where('slug', $roleSlug)->first();
+            if ($role === null)
+                continue;
+
+            foreach ($permissionSlugs as $permissionSlug) {
+                /** @var Permission $permission */
+                $permission = config('roles.models.permission')::where('slug', $permissionSlug)->first();
+                if ($permission === null)
+                    continue;
+
+                $role->{$method}($permission->{$permission->getKeyName()});
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->syncPermissions('detachPermission', $this->toAttach);
+        $this->syncPermissions('attachPermission', $this->toDetach);
+        $this->removeRoles();
+        $this->removePermissions();
+    }
+
+    /**
+     * Remove existing roles.
+     */
+    public function removeRoles(): void
+    {
+        foreach ($this->roles as $role) {
+            if (!array_key_exists('slug', $role))
+                $role['slug'] = Str::slug($role['name'], '.');
+
+            config('roles.models.role')::where('slug', $role['slug'])->forceDelete();
+        }
+    }
+
+    /**
+     * Remove existing permissions.
+     */
+    public function removePermissions(): void
+    {
+        foreach ($this->permissions as $permission) {
+            if (!array_key_exists('slug', $permission))
+                $permission['slug'] = Str::slug($permission['name'], '.');
+
+            config('roles.models.permission')::where('slug', $permission['slug'])->forceDelete();
+        }
+    }
+}
